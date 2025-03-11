b5da26f13b62cb57af4f8b37b9b544facbd86b0d
sreya1204 <sreyasivan12032004@gmail.com>
2025-03-11 15:55:18 +0530

    Update tutor.js

--- backend/models/tutor.js
+++ backend/models/tutor.js
@@ -1,1 +1,33 @@
+import mongoose from "mongoose";
 
+const tutorSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+    },
+    status: {
+        type: Boolean,
+    },
+    departmentName: {
+        type: String,
+        required: true,
+    },
+
+},
+    { timestamps: true }
+)
+
+const Tutor = mongoose.model("HOD",tutorSchema);
+export default Tutor
