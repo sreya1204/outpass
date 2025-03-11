6031598742d9a5ea85192146a9043716a843a685
sreya1204 <sreyasivan12032004@gmail.com>
2025-03-11 16:05:43 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,14 @@
+import express from "express"
+const router = express.Router()
 
+router.get('/login',(req,res)=>{
+    res.send('login page')
+})
+
+router.get('/logout',(req,res)=>{
+    res.send('logout page')
+})
+
+
+
+export default router
