52c14f30958b34c4a2126940bf56682b07dcd9ca
sreya1204 <sreyasivan12032004@gmail.com>
2025-03-11 15:53:19 +0530

    Update principal.js

--- backend/models/principal.js
+++ backend/models/principal.js
@@ -1,1 +1,33 @@
+import mongoose, { model } from "mongoose";
 
+const principalSchema  = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+    },
+    status: {
+        type: Boolean,
+        default: false
+    },
+    address: {
+        type: String
+    }
+}, {
+    timestamps: true
+})
+
+
+const Principal = mongoose.model("Principal",principalSchema);
+export default Principal
