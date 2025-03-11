b9a76595917f875fab39e64c300383eb4403e92d
sreya1204 <sreyasivan12032004@gmail.com>
2025-03-11 15:52:22 +0530

    Update HOD.js

--- backend/models/HOD.js
+++ backend/models/HOD.js
@@ -1,1 +1,33 @@
+import mongoose from "mongoose";
 
+const hodSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+    },
+    status: {
+        type: Boolean,
+    },
+    departmentName: {
+        type: String,
+        required: true,
+    },
+
+},
+    { timestamps: true }
+)
+
+const HOD = mongoose.model("HOD",hodSchema);
+export default HOD
