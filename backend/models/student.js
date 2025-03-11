d78967c6d41dffd0c354ce7b7c854e019aa43b53
sreya1204 <sreyasivan12032004@gmail.com>
2025-03-11 15:54:11 +0530

    Update student.js

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,1 +1,63 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
+const studentSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+        },
+        dateOfBirth: {
+            type: Date,
+        },
+        status: {
+            type: Boolean,
+            default: true,
+        },
+        regNumber: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        Batch: {
+            type: String,
+            required: true,
+        },
+        departmentName: {
+            type: String,
+            required: true,
+        },
+        departmentCode: {
+            type: String,
+        },
+        instituteCode: {
+            type: String,
+        },
+        currentSemester: {
+            type: Number,
+            required: true,
+        },
+        assignedTutor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tutor",
+        },
+    },
+    { timestamps: true }
+);
+
+
+const Student = mongoose.model("Student", studentSchema);
+
+export default Student;
